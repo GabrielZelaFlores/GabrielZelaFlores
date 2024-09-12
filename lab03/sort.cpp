06a195c0a73aded244a006ce265f6383558d2fbc
GabrielZelaFlores <gzelaf@ulasalle.edu.pe>
2024-09-11 19:35:34 -0500

    KickSort-Generando promedio de casos y imprimir tiempos de ordenamiento

--- lab03/sort.cpp
+++ lab03/sort.cpp
@@ -1,53 +1,59 @@
-// C++ program for implementation of Insertion Sort (Average Case)
 #include <iostream>
+#include <vector>
 #include <chrono>
-#include <algorithm> // for random_shuffle
-#include <cstdlib>   // for srand, rand
-#include <ctime>     // for time
+#include <cstdlib>  // Para rand()
+#include <ctime>    // Para time()
 using namespace std;
 
-/* Function to sort array using insertion sort */
-void insertionSort(int arr[], int n)
-{
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+// Función de partición
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
         }
-        arr[j + 1] = key;
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Implementación de la función QuickSort
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
-/* Function to generate average case for insertion sort (random shuffle) */
-void averageCase(int arr[], int n) {
-    srand(time(0));
+// Función para generar un caso promedio para QuickSort (arreglo aleatorio)
+void averagecase(vector<int>& arr, int n) {
+    srand(time(0));  // Semilla para generar diferentes valores aleatorios
     for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;
+        arr[i] = rand() % n + 1;  // Generar números aleatorios
     }
-    random_shuffle(arr, arr + n);  // Shuffle the array randomly
 }
 
-// Driver method
-int main()
-{
+int main() {
     int x;
     cin >> x;
-    
-    for (int i = 1; i <= x; i++) {
-        int b[i] = {};
 
-        // Generate average case
-        averageCase(b, i);
-        auto startAvg = chrono::high_resolution_clock::now();
-        insertionSort(b, i);
-        auto endAvg = chrono::high_resolution_clock::now();
-        auto durationAvg = chrono::duration_cast<chrono::nanoseconds>(endAvg - startAvg);
-        cout << "Average case duration (ns) for n = " << i << ": " << durationAvg.count() << endl;
+    for (int i = 1; i <= x; i++) {
+        vector<int> arr(i);
+
+        // Generar el caso promedio
+        averagecase(arr, i);
+
+        // Medir el tiempo de ejecución de QuickSort
+        auto inicio = chrono::high_resolution_clock::now();
+        quickSort(arr, 0, i - 1);
+        auto fin = chrono::high_resolution_clock::now();
+        auto duracion = chrono::duration_cast<chrono::nanoseconds>(fin - inicio);
+
+        cout << duracion.count() << endl;
     }
 
     return 0;
 }
-
