cf65c3db3202e975c26a412fcdac800488731812
GabrielZelaFlores <gzelaf@ulasalle.edu.pe>
2024-09-11 19:38:06 -0500

    Inserción-Generando mejores casos y imprimir tiempos de ordenamiento

--- lab03/sort.cpp
+++ lab03/sort.cpp
@@ -1,58 +1,46 @@
+// C++ program for implementation of Insertion Sort (Best Case)
 #include <iostream>
-#include <vector>
 #include <chrono>
-#include <cstdlib>  // Para rand()
-#include <ctime>    // Para time()
 using namespace std;
 
-// Función de partición
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-    }
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
-
-// Implementación de la función QuickSort
-void quickSort(vector<int>& arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        arr[j + 1] = key;
     }
 }
 
-// Función para generar un caso promedio para QuickSort (arreglo aleatorio)
-void averagecase(vector<int>& arr, int n) {
-    srand(time(0));  // Semilla para generar diferentes valores aleatorios
+/* Function to generate best case for insertion sort (sorted array) */
+void bestCase(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % n + 1;  // Generar números aleatorios
+        arr[i] = i + 1;  // Already sorted array
     }
 }
 
-int main() {
+// Driver method
+int main()
+{
     int x;
     cin >> x;
-
+    
     for (int i = 1; i <= x; i++) {
-        vector<int> arr(i);
+        int b[i] = {};
 
-        // Generar el caso promedio
-        averagecase(arr, i);
-
-        // Medir el tiempo de ejecución de QuickSort
-        auto inicio = chrono::high_resolution_clock::now();
-        quickSort(arr, 0, i - 1);
-        auto fin = chrono::high_resolution_clock::now();
-        auto duracion = chrono::duration_cast<chrono::nanoseconds>(fin - inicio);
-
-        cout << duracion.count() << endl;
+        // Generate best case
+        bestCase(b, i);
+        auto startBest = chrono::high_resolution_clock::now();
+        insertionSort(b, i);
+        auto endBest = chrono::high_resolution_clock::now();
+        auto durationBest = chrono::duration_cast<chrono::nanoseconds>(endBest - startBest);
+        cout << "Best case duration (ns) for n = " << i << ": " << durationBest.count() << endl;
     }
 
     return 0;
